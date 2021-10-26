09ea1d28af0831425a719c9082a6ac310abd626b
Edward Smale <essmale2005@gmail.com>
2021-10-26 13:02:18 +0100

    Update translation dumper

--- Dumpostor/Dumpers/TranslationsDumper.cs
+++ Dumpostor/Dumpers/TranslationsDumper.cs
@@ -1,14 +1,44 @@
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 
 namespace Dumpostor.Dumpers
 {
     public class TranslationsDumper : IDumper
     {
-        public string FileName => "translations.txt";
+        public string FileName => "translations.json";
 
         public string Dump()
         {
-            return TranslationController.Instance.Languages.Single(x => x.Name == "English").Data.text;
+            Dictionary<string, Dictionary<string, string>>
+                output = new Dictionary<string, Dictionary<string, string>>();
+
+            foreach (TranslatedImageSet translationSet in TranslationController.Instance.Languages)
+            {
+                Dictionary<string, string> translations = new Dictionary<string, string>();
+                string[] rows = translationSet.Data.text.Split("\r\n");
+
+                for (int i = 0; i < rows.Length; i++)
+                {
+                    if (i == 0) // skip header row
+                        continue;
+
+                    string row = rows[i];
+                    string[] columns = row.Split("\t");
+
+                    if (!translations.ContainsKey(columns[0]))
+                    {
+                        translations.Add(columns[0], columns[1]);
+                    }
+                }
+
+                if (!output.ContainsKey(translationSet.Name))
+                {
+                    output.Add(translationSet.Name, translations);
+                }
+            }
+            
+            return JsonSerializer.Serialize(output);
         }
     }
 }
